9a4b6b8434eb93f45e01b497a0279d1fbba4c4de
Rodion R <65biso2@gmail.com>
2022-11-17 23:59:52 +0300

    cosmetic changes

--- image_indexator_client/src/UI/components/ImageCardsList/ImageCardsList.jsx
+++ image_indexator_client/src/UI/components/ImageCardsList/ImageCardsList.jsx
@@ -4,6 +4,11 @@ import cl from './ImageCardsList.module.css';
 
 const ImageCardsList = ({imagesList,...props})=>{
     console.log(`Creating ImageCardsList with ${imagesList.length} images.`)
+    // if(imagesList.length==0){
+    //     imagesList = new Array(20);
+    //     for(var i =0; i<20;i++)
+    //         imagesList[i]={ url:'https://via.placeholder.com/150/f66b97', metadata:'https://via.placeholder.com/150/f66b97'};
+    // }
     return(
         <div className={cl.imageCardsList}>
             {imagesList ? imagesList.map((img,index) => 
